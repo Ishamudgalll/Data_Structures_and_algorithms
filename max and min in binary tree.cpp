133b8dac80a3b6ce8efdb3fcdd68f6214b76a4be
Ishamudgalll <47916019+Ishamudgalll@users.noreply.github.com>
2020-06-15 23:08:57 +0530

    Create max and min in binary tree.cpp

---
+++ max and min in binary tree.cpp
@@ -0,0 +1,14 @@
+int findMax(struct node* root)
+{
+if(!root)
+return INT_MIN;
+return max(root->data,max(findMax(root->left),findMax(root->right)));
+}
+
+
+int findMin(struct node* root)
+{
+if(!root)
+return INT_MAX;
+return min(root->data,min(findMin(root->right),findMin(root->left)));
+}
